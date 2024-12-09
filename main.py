48af54a1abe2f43c05e8d6fa023a63fcb2c1aa5f
Maxim Vassilev <Maxim.Vassilev@Maxim-Vassilevs-Mac.local>
2024-12-09 13:27:56 +0200

    Removed init_db method. Now the db is not created when starting the server

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import asyncio
 from src.grpc.config import DATABASE_URL as db_url
-from src.db.database import init_db
 from src.grpc.start_grpc_server import start_grpc_server
+from src.db.database import Database
 
 async def main():
-    db = await init_db(db_url)
+    db = Database(database_url=db_url)
     await start_grpc_server(db)
 
 if __name__ == '__main__':
--- src/db/database.py
+++ src/db/database.py
@@ -1,15 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from src.db.model import Base
 
 class Database:
     def __init__(self, database_url: str):
         self.engine = create_async_engine(database_url, echo=False, future=True)
         self.async_session = sessionmaker(self.engine, class_=AsyncSession, expire_on_commit=False)
-
-
-async def init_db(db_url):
-    database = Database(database_url=db_url)
-    async with database.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    return database
