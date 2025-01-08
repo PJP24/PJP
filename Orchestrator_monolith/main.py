966ffee4ad465d657c342e4c1fa229b8c4d057d1
Max Vassilev <maximvassilev01@gmail.com>
2025-01-08 16:01:42 +0200

    Separate GraphQL from Orchestrator

--- Orchestrator_monolith/main.py
+++ Orchestrator_monolith/main.py
@@ -1,5 +1,5 @@
-import uvicorn
-from src.graphql.app import app
+# import uvicorn
+# from src.graphql.app import app
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5001)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=5001)
--- Orchestrator_monolith/src/orchestrator/orchestrator.py
+++ Orchestrator_monolith/src/orchestrator/orchestrator.py
@@ -6,16 +6,12 @@ class Orchestrator:
         self.host = "subscription_server_container:50052"
 
     async def get_all_subscriptions(self):
-        from src.graphql.schema import Subscription
-
         async with grpc.aio.insecure_channel(self.host) as channel:
             stub = subscription_pb2_grpc.SubscriptionServiceStub(channel)
             request = subscription_pb2.GetSubscriptionsRequest()
             response = await stub.GetSubscriptions(request)
-    
-        subscriptions = [Subscription(email=sub.email, subscription_type=sub.subscription_type, is_active=sub.is_active) for sub in response.subscriptions]
 
-        return subscriptions
+        return [{"email": sub.email, "subscription_type": sub.subscription_type, "is_active": sub.is_active} for sub in response.subscriptions]
     
     async def add_subscription(self, email: str, subscription_type: str):
         async with grpc.aio.insecure_channel(self.host) as channel:
